3de8f801891efd0127f5edfd2db6f80dabd65fa9
Robbie Reta <robbie.reta67@gmail.com>
2021-07-12 10:43:49 -0500

    mod

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.all("/*", function(req, res, next) {
   // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   // Set custom headers for CORS
   res.header(
